81a6280704e5093e30f9571aafd0790b835d3e86
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2024-09-29 17:39:43 -0500

    Verified serialization with JSON

--- api/test/src/bankerx/api/CodecsSpec.scala
+++ api/test/src/bankerx/api/CodecsSpec.scala
@@ -3,6 +3,7 @@ import org.scalatest.*
 import wordspec.* 
 import matchers.*
 import bankerx.API.*
+import bankerx.api.fdc3
 import com.github.plokhotnyuk.jsoniter_scala.macros.*
 import com.github.plokhotnyuk.jsoniter_scala.core.*
 
@@ -35,8 +36,53 @@ class CodecsSpec extends AnyWordSpec with should.Matchers with Codecs:
                     12
                 )
                 val json = writeToString(terms)
+                println(json)
                 val decodedTerms = readFromString[Terms](json)
                 decodedTerms.shouldEqual(terms)
+            }
+        }
+
+        "given a GetTermsIntent" should {
+            "be able to encode and decode it" in {
+                val getTermsIntent = fdc3.GetTermsIntent(
+                    fdc3.IntentName("intent1"),
+                    fdc3.Source("source1"),
+                    fdc3.Destination("destination1"),
+                    fdc3.GetTermsRequestPayload(
+                        fdc3.PayloadType("payloadType1"),
+                        Purchase(
+                            100,
+                            "Vender1",
+                            java.time.LocalDate.now(),
+                            java.time.LocalTime.now(),
+                            "user 1",
+                            Fuel,
+                            "pointOfSale 1"
+                        )
+                    )
+                )
+                val json = writeToString(getTermsIntent)
+                println(json)
+                val decodedGetTermsIntent = readFromString[fdc3.GetTermsIntent](json)                
+                assert(decodedGetTermsIntent == getTermsIntent)
+            }
+        }
+
+        "given a GetTermsResponsePayload" should {
+            "be able to encode and decode it" in {
+                val getTermsResponsePayload = fdc3.GetTermsResponsePayload(
+                    fdc3.PayloadType("payloadType1"),
+                    Terms(
+                        "Provider1",
+                        100,
+                        0.1,
+                        12
+                    )
+                )
+                val json = writeToString(getTermsResponsePayload)
+                println(json)
+                val decodedGetTermsResponsePayload = readFromString[fdc3.GetTermsResponsePayload](json)
+                assert(decodedGetTermsResponsePayload == getTermsResponsePayload)
             }
         }
     }    
